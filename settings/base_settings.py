677a92d45bbeea38e423db53d449a47e6b4f4b86
Aaron Riedener <aaron.riedener@gmail.com>
2024-04-06 18:45:57 +0200

    Update paths for STATIC_ROOT and MEDIA_ROOT

--- settings/base_settings.py
+++ settings/base_settings.py
@@ -91,10 +91,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 PROJECT_ROOT = BASE_DIR
 
